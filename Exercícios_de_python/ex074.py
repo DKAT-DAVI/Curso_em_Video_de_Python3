ce71936eb81d35c60765d92a1bdbd07ccf4e954a
DKAT <eudkatdavikalel@gmail.com>
2021-08-27 21:36:43 -0300

    exercícios 074 e 075

--- Exercícios_de_python/ex074.py
+++ Exercícios_de_python/ex074.py
@@ -1,3 +1,14 @@
 print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
 print('\033[1;31m     MENOR E MAIOR VALORES\033[m')
-print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
+
+from random import randint
+
+num = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10),
+       randint(0, 10))
+
+print(f'Os valores sorteados foram {num}')
+print(f'O maior valor foi {max(num)}')
+print(f'O menor valor foi {min(num)}')
+
+
+
--- Exercícios_de_python/ex075.py
+++ Exercícios_de_python/ex075.py
@@ -1,3 +1,25 @@
 print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
 print('\033[1;31m      DADOS EM UMA TUPLA\033[m')
 print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
+num = (int(input('Digite um número: ')), int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')), int(input('Digite o último número: ')))
+impar = 0
+print(f'Você digitou os números: {num}')
+print(f'O número 9 apareceu {num.count(9)} vezes')
+try:
+       print(f'O números 3 foi digitado na {num.index(3)+1}ª posição')
+except:
+       print('Não foi digitado número 3')
+print('Os valores pares digitados foram: ', end='')
+for n in num:
+       if n % 2 != 0:
+              impar += 1
+if impar == 4:
+       print('Não foi digitado nenhum número par')
+else:
+       for n in num:
+              if n % 2 == 0:
+                     print(n, end=' ')
+
+
+
