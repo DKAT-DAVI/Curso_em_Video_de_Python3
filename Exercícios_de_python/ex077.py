cf05348dba1b2bd479448092a85af87115959e97
DKAT <eudkatdavikalel@gmail.com>
2021-08-31 20:05:29 -0300

    exercícios 077

--- Exercícios_de_python/ex077.py
+++ Exercícios_de_python/ex077.py
@@ -1,3 +1,10 @@
 print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
 print('\033[1;31m       CONTANDO VOGAIS\033[m')
 print('\033[1;93m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m')
+words = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS',
+         'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMAR', 'FUTURO')
+for pos in words:
+    print(f'\nNa palavra {pos} temos as vogais: ', end='')
+    for letra in pos:
+        if letra.lower() in 'aeiou':
+            print(letra.lower(), end=' ')
