a9e36670151343749b5c020e0091ccb43472d051
DKAT <eudkatdavikalel@gmail.com>
2021-12-06 14:36:06 -0300

    arrumando repositório

---
+++ Mundo_03_Python/Listas.py
@@ -0,0 +1,21 @@
+#Replace value in list
+num = [2, 5, 9, 1]
+print(num[2])
+num[2] = 'linux'
+print(num[2])
+print(num)
+# Add value to the list
+num.append('windows')
+print(num)
+#Arrange the list in ascending order(only int variables)
+var = [8, 6, 32, 11, 5]
+print(var)
+var.sort()
+var.append(2)
+var.insert(2, 2)
+var. insert(0, 2)
+print(var)
+while 2 in var:
+    var.remove(2)
+    print(var)
+
